7ac5f5c623c38922d10126da6f981db3b5f2756d
upupu <etienne.lemonnier44@gmail.com>
2020-03-24 16:05:35 +0100

    oui

--- formulaire.php
+++ formulaire.php
@@ -1,1 +1,17 @@
 <?php
+
+class Formulaire {
+    public function __construct($nomFichier){
+        echo "<form>";
+    }
+    public function ajouterZoneTexte(){
+        echo "<input type='text'>";
+    }
+    public function ajouterBouton(){
+        echo "<input type='submit'>";
+    }
+    public function getForm(){
+        $this->ajouterZoneTexte();
+        $this->ajouterBouton();
+    }
+}
